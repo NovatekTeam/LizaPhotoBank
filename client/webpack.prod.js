d23d3becf7febb144309e69277c755214ee9c248
Gogs <gogs@fake.local>
2022-06-24 03:00:39 +0500

    lalala

--- client/webpack.prod.js
+++
@@ -1,9 +0,0 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const webpack = require("webpack")
-
-module.exports = merge(common, {
-    plugins: [
-        new webpack.EnvironmentPlugin(['GRAPHQL_SERVER']),
-    ]
-});
