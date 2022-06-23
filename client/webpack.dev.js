ee87f410b87d2cab182223168fba0d573fbea42f
igor.abrashin <igor.abrashin@novatek.ru>
2022-06-24 02:02:40 +0500

    client env fixes

---
+++ client/webpack.dev.js
@@ -0,0 +1,11 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = merge(common, {
+    plugins: [
+        new Dotenv({
+            path: '.env'   
+        })
+    ]
+});
