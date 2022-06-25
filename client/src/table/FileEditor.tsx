3790991d1a33a2e483a3d84de16339ee045507e2
NYKuzmin <NYKuzmin@novatek.ru>
2022-06-26 03:31:42 +0500

    save tags

--- client/src/table/FileEditor.tsx
+++ client/src/table/FileEditor.tsx
@@ -1,10 +1,15 @@
 import React from "react";;
-import {Input, Modal, Popover, Tag, Tooltip} from "antd";
+import {Input, Modal, Popover, Select, Tag, Tooltip} from "antd";
 import {CalendarTwoTone, PlusOutlined} from "@ant-design/icons";
 import {TweenOneGroup} from "rc-tween-one";
 import {useEffect, useRef, useState} from "react";
+import {usefilterTree} from "../tags/__generated__/filterTree";
+import {useupdateDb} from "./__generated__/updateDb";
+import {useupdateSolr} from "./__generated__/updateSolr";
+import {TagsWhereUniqueInput} from "../__generated__/types";
 
 export const FileEditor = (props: {selectedRow: {
+        id: number,
         media_name: string,
         media_path: string,
         media_type: string,
@@ -12,23 +17,19 @@ export const FileEditor = (props: {selectedRow: {
         media_preview_url: string
     }}) => {
 
+    let [updateDb] = useupdateDb();
+    let [updateSolr] = useupdateSolr()
+
+    let {data} = usefilterTree()
+
 
     const [selectedRow, setSelectedRow] = useState(undefined)
+    const [selectedTags, setSelectedTags] = useState([])
+
 
     useEffect(() => {
         setSelectedRow(props.selectedRow);
     }, [props.selectedRow])
-
-    const [inputTagVisible, setInputTagVisible] = useState(false);
-    const [inputTagValue, setInputTagValue] = useState('');
-
-    const inputRef = useRef(null);
-    useEffect(() => {
-        if (inputTagVisible) {
-            inputRef.current?.focus();
-            inputRef.current?.focus();
-        }
-    }, []);
 
 
     function popupImage(preview: string) {
@@ -37,29 +38,43 @@ export const FileEditor = (props: {selectedRow: {
         )
     }
 
-    function handleInputConfirm() {
-        if (inputTagValue && selectedRow.media_tags.indexOf(inputTagValue) === -1) {
-            selectedRow.media_tags = [...selectedRow.media_tags, inputTagValue]
-        }
-
-        setInputTagVisible(false);
-        setInputTagValue('');
+    function findTags() {
+        return data?.TagsQuery.filter(it => selectedRow?.media_tags?.include(it.tagName));
     }
 
+    const handleChange = (value: any[]) => {
+        let tags = value.map(idx => data?.TagsQuery[idx]);
+        setSelectedTags(tags)
+    };
 
-    function removeTag(removedTag) {
-        const newTags = selectedRow.media_tags.filter((tag) => tag !== removedTag);
-        console.log(newTags);
-        setSelectedRow({
-            ...selectedRow,
-            media_tags: newTags
+    function handleSave() {
+
+       updateDb({
+           variables : {
+               mediaId: +selectedRow?.id,
+               tagsId: selectedTags?.map(it => {
+                   const tag: TagsWhereUniqueInput = {
+                       id: +it.id
+                   };
+                   return tag;
+               })
+           }
+       });
+        updateSolr({
+            variables: {
+                mediaId: selectedRow?.id,
+                tags: selectedTags?.map(it => it.tagName)
+            }
         })
-        console.log("remove tag " + removedTag);
+        setSelectedRow(undefined)
+        setSelectedTags([])
     }
-
 
     return (
-        <Modal title={selectedRow?.media_name} visible={selectedRow} onOk={() => setSelectedRow(undefined)} onCancel={() => setSelectedRow(undefined)} >
+        <Modal title={selectedRow?.media_name} visible={selectedRow} onOk={handleSave} onCancel={() => {
+            setSelectedRow(undefined)
+            setSelectedTags([])
+        }} >
             <Popover content={() => popupImage(selectedRow?.media_preview_url)} title={selectedRow?.media_name}>
                 <img style={{marginBottom: 16}}  width={150} height={150} src={selectedRow?.media_preview_url} alt={selectedRow?.media_preview_url}/>
             </Popover>
@@ -68,60 +83,21 @@ export const FileEditor = (props: {selectedRow: {
                     marginBottom: 8,
                 }}
             >
-                <TweenOneGroup
-                    enter={{
-                        scale: 0.8,
-                        opacity: 0,
-                        type: 'from',
-                        duration: 100,
+                <Select
+                    mode="multiple"
+                    allowClear
+                    style={{
+                        width: '100%',
                     }}
-                    onEnd={(e) => {
-                        if (e.type === 'appear' || e.type === 'enter') {
-                            e.target.style = 'display: inline-block';
-                        }
-                    }}
-                    leave={{
-                        opacity: 0,
-                        width: 0,
-                        scale: 0,
-                        duration: 200,
-                    }}
-                    appear={false}
+                    placeholder="Please select"
+                    defaultValue={findTags()}
+                    onChange={handleChange}
                 >
                     {
-                        selectedRow?.media_tags?.map(it =>
-                            <Tag
-                                closable
-                                onClose={(e) => {
-                                    e.preventDefault();
-                                    removeTag(it);
-                                }}
-                            >
-                                {it}
-                            </Tag>
-                        )
+                        data?.TagsQuery?.map(it => <Select key={it.id}>{it.tagName}</Select>)
                     }
-                </TweenOneGroup>
+                </Select>
             </div>
-            {inputTagVisible && (
-                <Input
-                    ref={inputRef}
-                    type="text"
-                    size="small"
-                    style={{
-                        width: 78,
-                    }}
-                    value={inputTagValue}
-                    onChange={(e) =>  setInputTagValue(e.target.value)}
-                    onBlur={handleInputConfirm}
-                    onPressEnter={handleInputConfirm}
-                />
-            )}
-            {!inputTagVisible && (
-                <Tag onClick={() => setInputTagVisible(true)} style={{ background: '#fff',  borderStyle: 'dashed'}}>
-                    <PlusOutlined /> Добавить
-                </Tag>
-            )}
         </Modal>
     )
 }
