4126f5492406aea1d0277ed4d115d7b057a745a8
vjiastelin <vjiastelin@gmail.com>
2022-06-26 12:45:07 +0500

    handle sync ydisk

--- client/src/buttonsPanel/ButtonPanel.tsx
+++ client/src/buttonsPanel/ButtonPanel.tsx
@@ -1,38 +1,41 @@
 import { Button } from "antd"
 import React, { Fragment } from "react"
 import { usesyncDb, usesyncDbLazyQuery } from "./__generated__/syncWithDb"
-import { usesyncYdisk } from "./__generated__/syncWithYdisk"
+import { usesyncYdisk, usesyncYdiskLazyQuery } from "./__generated__/syncWithYdisk"
 
 export const ButtonPanel: React.FC = () => {
 
-    function syncDb() {        
-      
-        startSync().then(data => 
-            alert(data.data?.syncSolrWDb)   
+    function syncDb() {
+
+        startSync().then(data =>
+            alert(data.data?.syncSolrWDb)
         )
-        
-        
-        
+
+
+
     }
 
-function syncYdisk() {
-    //const {data,loading,error} = usesyncYdisk()
-    alert('Files synced')
-}
+    function syncYdisk() {
+        startSyncYdisk().then(
+            res => alert(res.data?.syncFiles)
+        )
+
+    }
 
 
-function handleAuto() {
+    function handleAuto() {
+        alert('Auto marking starting')
+    }
 
-}
+    const [startSync] = usesyncDbLazyQuery()
+    const [startSyncYdisk] = usesyncYdiskLazyQuery()
 
-const [startSync] = usesyncDbLazyQuery()
 
-
-return (
-    <Fragment>
-        <Button type="primary" style={{ marginLeft: 16, marginRight: 16, marginTop: 16 }} onClick={syncYdisk}>Синхронизация с облаком</Button>
-        <Button type="primary" style={{ marginLeft: 16, marginRight: 16, marginTop: 16 }} onClick={syncDb}>Синхронизация с индексом</Button>
-        <Button type="primary" style={{ marginLeft: 16, marginRight: 16, marginTop: 16 }} onClick={handleAuto}>Авто разметка</Button>
-    </Fragment>
-)
+    return (
+        <Fragment>
+            <Button type="primary" style={{ marginLeft: 16, marginRight: 16, marginTop: 16 }} onClick={syncYdisk}>Синхронизация с облаком</Button>
+            <Button type="primary" style={{ marginLeft: 16, marginRight: 16, marginTop: 16 }} onClick={syncDb}>Синхронизация с индексом</Button>
+            <Button type="primary" style={{ marginLeft: 16, marginRight: 16, marginTop: 16 }} onClick={handleAuto}>Авто разметка</Button>
+        </Fragment>
+    )
 }
