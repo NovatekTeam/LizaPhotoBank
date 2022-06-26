a90f99710b02a9c88606b6f5a988239f3fbb5f3d
vjiastelin <vjiastelin@gmail.com>
2022-06-26 12:05:30 +0500

    syncDb by  button

--- client/src/buttonsPanel/ButtonPanel.tsx
+++ client/src/buttonsPanel/ButtonPanel.tsx
@@ -1,31 +1,38 @@
 import { Button } from "antd"
-import React,{ Fragment } from "react"
-import { usesyncDb } from "./__generated__/syncWithDb"
+import React, { Fragment } from "react"
+import { usesyncDb, usesyncDbLazyQuery } from "./__generated__/syncWithDb"
 import { usesyncYdisk } from "./__generated__/syncWithYdisk"
 
-export const ButtonPanel : React.FC = () => {
+export const ButtonPanel: React.FC = () => {
 
-    function syncDb(){
-        console.log('afaf')
-        // const {data,loading,error} = usesyncDb()
-        // alert(data)
+    function syncDb() {        
+      
+        startSync().then(data => 
+            alert(data.data?.syncSolrWDb)   
+        )
+        
+        
+        
     }
 
-    function syncYdisk(){
-        //const {data,loading,error} = usesyncYdisk()
-        alert('Files synced')
-    }
+function syncYdisk() {
+    //const {data,loading,error} = usesyncYdisk()
+    alert('Files synced')
+}
 
 
-    function handleAuto() {
+function handleAuto() {
 
-    }
+}
 
-    return (
-        <Fragment>
-            <Button type="primary" style={{margin: 16}} onClick={syncYdisk}>Синхронизация с облаком</Button>
-            <Button type="primary" style={{margin: 16}} onClick={syncDb}>Синхронизация с индексом</Button>
-            <Button type="primary" style={{margin: 16}} onClick={handleAuto}>Авто разметка</Button>
-        </Fragment>
-    )
+const [startSync] = usesyncDbLazyQuery()
+
+
+return (
+    <Fragment>
+        <Button type="primary" style={{ marginLeft: 16, marginRight: 16, marginTop: 16 }} onClick={syncYdisk}>Синхронизация с облаком</Button>
+        <Button type="primary" style={{ marginLeft: 16, marginRight: 16, marginTop: 16 }} onClick={syncDb}>Синхронизация с индексом</Button>
+        <Button type="primary" style={{ marginLeft: 16, marginRight: 16, marginTop: 16 }} onClick={handleAuto}>Авто разметка</Button>
+    </Fragment>
+)
 }
